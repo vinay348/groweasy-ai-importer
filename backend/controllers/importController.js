64e8c77a7a434b3107250c8a10fcc1a6df7ec178
Vinay Kumar <vinaykumar.gsl225@gmail.com>
2026-07-08 20:42:54 +0530

    Complete Postman testing setup

--- backend/controllers/importController.js
+++ backend/controllers/importController.js
@@ -1,13 +1,25 @@
+import { parseCSV } from "../services/csvService.js";
+
 export const importCSV = async (req, res) => {
   try {
-    console.log(req.file);
+    if (!req.file) {
+      return res.status(400).json({
+        success: false,
+        message: "CSV file is required",
+      });
+    }
 
-    res.json({
+    const records = await parseCSV(req.file.path);
+
+    res.status(200).json({
       success: true,
-      message: "CSV uploaded successfully",
+      totalRecords: records.length,
+      data: records,
     });
 
   } catch (err) {
+    console.error(error);
+
     res.status(500).json({
       success: false,
       message: err.message,
---
+++ backend/services/csvService.js
@@ -0,0 +1,20 @@
+import fs from "fs";
+import csv from "csv-parser";
+
+export const parseCSV = (filePath) => {
+  return new Promise((resolve, reject) => {
+    const results = [];
+
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on("data", (data) => {
+        results.push(data);
+      })
+      .on("end", () => {
+        resolve(results);
+      })
+      .on("error", (err) => {
+        reject(err);
+      });
+  });
+};
